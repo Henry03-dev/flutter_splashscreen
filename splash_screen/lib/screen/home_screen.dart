c3935440d5905cc053922280aef0023355e60dc9
Henry03-dev <85604492+Henry03-dev@users.noreply.github.com>
2025-03-25 14:33:01 +0000

    25_0325_2

--- splash_screen/lib/screen/home_screen.dart
+++ splash_screen/lib/screen/home_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+
+  WebViewController controller = WebViewController();
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: Text('Code Factory'),
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        body: WebViewWidget(
+          controller: controller,
+        ),
     );
   }
 }
