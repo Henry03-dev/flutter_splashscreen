e226ec5cd0e1054f85b2c3034b64d7e1f8e1f3ba
Henry03-dev <85604492+Henry03-dev@users.noreply.github.com>
2025-03-27 11:24:30 +0000

    25_0327_1

--- splash_screen/lib/screen/home_screen.dart
+++ splash_screen/lib/screen/home_screen.dart
@@ -1,46 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class HomeScreen extends StatefulWidget {
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  Color color = Colors.blue;
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    print('build 실행!');
-
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: (){
-                if (color == Colors.blue) {
-                  color = Colors.red;
-                } else {
-                  color = Colors.blue;
-                }
+      body: CodeFactoryWidget(),
+    );
+  }
+}
 
-                print('색상 변경! color: $color');
-                setState((){}); // setState로 빌드함수 재실행
-              },
-              child: Text('색상 변경!'),
-            ),
-            SizedBox(height: 32.0),
-            Container(
-              width: 50.0,
-              height: 50.0,
-              color: color,
-            ),
-          ],
-        ),
-      ),
+class CodeFactoryWidget extends StatelessWidget {
+  CodeFactoryWidget({super.key}) {
+    print('--- 생성자 ---');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('--- 빌드 ---');
+
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: Colors.red,
     );
   }
 }
