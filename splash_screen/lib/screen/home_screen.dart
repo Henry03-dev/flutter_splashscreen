f23db2de52f379c10dd92f157eba8a407ba05082
Henry03-dev <85604492+Henry03-dev@users.noreply.github.com>
2025-04-15 14:22:22 +0000

    25_0415_1

--- splash_screen/lib/screen/home_screen.dart
+++ splash_screen/lib/screen/home_screen.dart
@@ -2,13 +2,108 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:async';
 
+import 'package:splash_screen/const/color.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      backgroundColor: primaryColor,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              /// 제목과 아이콘 버튼이 있는곳
+              _Header(),
+
+              /// 숫자가 있는곳
+              _Body(),
+
+              /// 버튼이 있는곳
+              _Footer(),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '랜덤숫자 생성기',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        IconButton(
+          color: redColor,
+          onPressed: () {},
+          icon: Icon(Icons.settings),
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      // Expanded로 최대로 확장하기
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          [1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+        ]
+          .map(
+            (e) => Row(
+              children: e
+                .map(
+                  (number) => Image.asset(
+                    'asset/img/$number.png',
+                    width: 50.0,
+                    height: 70.0,
+                  ),
+                ).toList(),
+            ),
+        )
+        .toList(),
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  const _Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: redColor,
+        foregroundColor: Colors.white,
+      ),
+      child: Text('생성하기!'),
     );
   }
 }
