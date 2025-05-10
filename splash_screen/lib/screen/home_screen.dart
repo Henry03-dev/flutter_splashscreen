7e72545bb88c4a89784f1970ec8589dd595e4b37
Henry03-dev <85604492+Henry03-dev@users.noreply.github.com>
2025-05-10 13:04:53 +0000

    25_0510_1

--- splash_screen/lib/screen/home_screen.dart
+++ splash_screen/lib/screen/home_screen.dart
@@ -1,166 +1,31 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:video_player/video_player.dart';
+import 'package:splash_screen/screen/route_one_screen.dart';
+import 'package:splash_screen/layout/default_layout.dart';
 
-import 'dart:io';
-
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  XFile? video;
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body:
-          video != null 
-          ? _VideoPlayer(
-              video: video!,
-          ) 
-          : _VideoSelecter(onLogoTap: onLogoTap),
-    );
-  }
-
-  onLogoTap() async {
-    final video = await ImagePicker().pickVideo(source: ImageSource.gallery);
-
-    setState(() {
-      this.video = video;
-    });
-  }
-}
-
-class _VideoSelecter extends StatelessWidget {
-  final VoidCallback onLogoTap;
-
-  const _VideoSelecter({required this.onLogoTap, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          // 그라디언트 방향
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-
-          colors: [
-            // 그라디언트 색상
-            Color(0xFF2A3A7C),
-            Color(0xFF000118),
-          ],
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [_Logo(onTap: onLogoTap), SizedBox(height: 28.0), _Title()],
-      ),
-    );
-  }
-}
-
-class _Logo extends StatelessWidget {
-  final VoidCallback onTap;
-
-  const _Logo({required this.onTap, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Image.asset('asset/img/logo.png'),
-    );
-  }
-}
-
-class _Title extends StatelessWidget {
-  const _Title({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final textStyle = TextStyle(
-      color: Colors.white,
-      fontSize: 32.0,
-      fontWeight: FontWeight.w300,
-    );
-
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return DefaultLayout(
+      title: 'HomeScreen',
       children: [
-        Text('VIDEO', style: textStyle),
-
-        Text(
-          'PLAYER',
-          style: textStyle.copyWith(
-            // copyWith을 통해
-            // 기존 textStyle을 복사하여
-            // fontWeight만 변경
-            fontWeight: FontWeight.w700,
-          ),
+        OutlinedButton(
+          onPressed: () async {
+            final result = await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) {
+                  return const RouteOneScreen(
+                    number: 20,
+                  );
+                },
+              ),
+            );
+            print(result);
+          },
+          child: Text('Push'),
         ),
       ],
     );
   }
-}
-
-class _VideoPlayer extends StatefulWidget {
-  final XFile video;
-  const _VideoPlayer({
-    required this.video,
-    super.key
-    });
-
-  @override
-  State<_VideoPlayer> createState() => _VideoPlayerState();
-}
-
-class _VideoPlayerState extends State<_VideoPlayer> {
-  late final VideoPlayerController videoPlayerController;
-  // late > 해당 값이 사용되기 전에 무조건 초기화
-
-  @override
-  void initState() {
-    super.initState();
-
-    initializeController();
-  }
-
-  initializeController() async {
-    videoPlayerController = VideoPlayerController.file(
-      File(
-        widget.video.path,
-      ),
-    );
-
-    await videoPlayerController.initialize();
-
-    setState(() {}); 
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: AspectRatio(
-        aspectRatio: videoPlayerController.value.aspectRatio,
-        child: VideoPlayer(
-          videoPlayerController,
-        ),
-      ),
-    );
-  }
-
-  @override
-void dispose() {
-  videoPlayerController.dispose();
-  super.dispose();
-}
 }
